439523ee121e72cd4bdc3cbdf024ec9127c504ed
CheesyBoy03 <57229908+CheesyBoy03@users.noreply.github.com>
2020-01-03 17:39:56 +0400

    Initial commit

---
+++ Split Strings.py
@@ -0,0 +1,15 @@
+s = str(input('Введите строку: '))
+def solution(s):
+    if s == '':
+        s = []
+        print(s)
+    else:
+        s_list = [s[i:i + 2] for i in range(0, len(s), 2)]
+        if len(s)%2 == 1:
+            last = s_list[-1]
+            l = last + '_'
+            s_list.pop(-1)
+            s_list.append(l)
+            
+        print(s_list)
+solution(s)
